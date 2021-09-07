9fc3c879736880cc83bd2e7c939f6b791ece54ce
zerbash <dbasham@overlake.org>
2021-09-07 13:31:33 -0700

    fleshes out exercise 1&2

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -1,20 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("words.txt"));
-        String[] words = new String[1000];
-        int i = 0;
         ArrayList<String> words2 = new ArrayList<>();
         while (scanner.hasNext()) {
-            words[i] = scanner.next();
-            i++;
+            String word = scanner.next();
+            words2.add(word);
         }
-        for (int j = i - 1; j >= 0; j--) {
-            System.out.println(words[j]);
-        }
+
+//        Collections.reverse(words2);
+//        for (String word: words2) {
+//            System.out.println(word);
+
+//        for (String word: words2) {
+//            System.out.println(uppercasePlurals(word));
+//        }
+//
+//
     }
+//
+//    private static String uppercasePlurals(String word) {
+//        if (word.endsWith("s")) {
+//            String capital = word.substring(0, 1).toUpperCase() + word.substring(1);
+//            return capital;
+//        } else {
+//            return word;
+//        }
+//    }
 }
