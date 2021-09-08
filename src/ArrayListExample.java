4adcdb18c3ced3579e7a36bac154aa0e02e20273
zerbash <dbasham@overlake.org>
2021-09-08 14:24:33 -0700

    finish ArrayList examples

--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -20,16 +20,37 @@ public class ArrayListExample {
 //        for (String word: words2) {
 //            System.out.println(uppercasePlurals(word));
 //        }
+
+//        words2.removeIf(s->s.endsWith("s"));
 //
-//
-    }
-//
-//    private static String uppercasePlurals(String word) {
-//        if (word.endsWith("s")) {
-//            String capital = word.substring(0, 1).toUpperCase() + word.substring(1);
-//            return capital;
-//        } else {
-//            return word;
+//        for (int i = 0; i < words2.size() - 1; i++){
+//            if (words2.get(i).endsWith("s")) {
+//                words2.remove(i);
+//                i--;
+//            }
 //        }
-//    }
+//
+//        for (String word: words2){
+//            System.out.println(word);
+//        }
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        Integer five = new Integer(5);
+        integers.add(five);
+        integers.add(5);
+        printInt(integers.get(0));
+    }
+
+    public static void printInt(int num) {
+        System.out.println(num);
+    }
+
+    private static String uppercasePlurals(String word) {
+        if (word.endsWith("s")) {
+            String capital = word.substring(0, 1).toUpperCase() + word.substring(1);
+            return capital;
+        } else {
+            return word;
+        }
+    }
 }
